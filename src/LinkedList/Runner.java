37fe85699a343b9737b0d36d85be612dd1fcc9bd
vinitparekh17 <vinitparekh1742@gmail.com>
2024-07-18 20:55:38 +0530

    linked list

--- src/LinkedList/Runner.java
+++ src/LinkedList/Runner.java
@@ -1,19 +1,13 @@
 package LinkedList;
 
-
 public class Runner {
     public static void main(String[] args) {
         Linkedlist linkedlist = new Linkedlist();
         linkedlist.insert(5);
         linkedlist.insert(10);
-        linkedlist.insert(15);
-
         linkedlist.insertAtHead(3);
-
         linkedlist.insertAt(1, 11);
-
         linkedlist.deleteAt(1);
-
         linkedlist.show();
     }
 }
